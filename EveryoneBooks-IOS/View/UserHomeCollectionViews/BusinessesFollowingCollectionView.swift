9d0a3e089579ca655f04ba3feaac813327f8e06f
Michael Perrone <mikesdatascience@yahoo.com>
2021-02-17 02:03:47 -0500

    dwd

---
+++ EveryoneBooks-IOS/View/UserHomeCollectionViews/BusinessesFollowingCollectionView.swift
@@ -0,0 +1,23 @@
+//
+//  BusinessesFollowingCollectionView.swift
+//  EveryoneBooks-IOS
+//
+//  Created by Michael Perrone on 12/8/20.
+//  Copyright © 2020 Michael Perrone. All rights reserved.
+//
+
+import UIKit
+
+class BusinessesFollowingCollectionView: UICollectionView {
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout());
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented");
+    }
+    
+    
+}
